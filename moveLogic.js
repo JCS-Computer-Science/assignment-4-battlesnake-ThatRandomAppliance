52ada30c2c629b892f0963c318647fc3800589d2
Owen Morgan <owenmorgan089@gmail.com>
2025-04-30 17:56:46 +0000

    floodfill works better

--- moveLogic.js
+++ moveLogic.js
@@ -1,8 +1,3 @@
-// import * as PF from 'pathfinding'
-
-// let finder = new PF.AStarFinder()
-    let snekBody;
-    let snekHead;
     
     function gridMaker(height,width,myBody,sneks,defaultVal = 1) {
         let gridUC = [];
@@ -38,81 +33,62 @@
 }
 
 
-    function floodFill(grid, sRow, sClm, newColor) {
-        const oldColor = grid[sRow][sClm];
-        
-        // If the starting pixel already has the new color, return
-        if (oldColor === newColor) {
-            return grid;
-        }
-    
-        // Dimensions of the grid
-        const rows = grid.length;
-        const cols = grid[0].length;
-        // let pthGrid = gridMaker(cols,rows)
-    
-        // Queue for BFS
-        const q = [[sRow, sClm]];
-    
-        // Change the starting pixel's color
-        grid[sRow][sClm] = newColor;
-    
-        // Direction vectors for 4 adjacent directions
-        const directions = [ [1, 0], [-1, 0], [0, 1], [0, -1] ];
-    
-        // BFS loop
-        while (q.length > 0) {
-            const [x, y] = q.shift();
-    
-            for (const [dx, dy] of directions) {
-                const nx = x + dx;
-                const ny = y + dy;
-    
-                // Check boundary conditions and color match
-                if (
-                    nx >= 0 && nx < rows && 
-                    ny >= 0 && ny < cols && 
-                    grid[nx][ny] === oldColor
-                ) {
-                    // Change color and add to queue
-                    grid[nx][ny] = newColor;
-                    q.push([nx, ny]);
-                }
-            }
-        }
-    
-        return grid;
+function floodFill(grid, sR, sC, newC) {
+    let origC = grid[sR][sC]
+    if (origC==newC) {
+        return grid
     }
+    fill(grid, sR, sC, newC,origC)
+    return grid
+}
+function fill(grid, sR, sC, newC, origC){
+    if (sR < 0 || sC < 0 || sR > grid.length-1 || sC > grid[0].length-1 || grid[sR][sC]!==origC) {
+        return;
+    }
+    else {
+        grid[sR][sC]=newC
+    }
+    fill(grid, sR - 1, sC, newC, origC);
 
-    function movePicker(pathGrid,startY,startX,moveSafety) {
-        console.log(moveSafety);
-        
-        // for (let m = 0; m < 4; m++) {
-            // console.log(pathGrid[startY][startX+1]);
-            if (startX+1<=10||startX-1>=0) {
-                if (pathGrid[startY][startX]+1==1) {
-                    moveSafety.right=true
-                    console.log("right: "+moveSafety);
-                    
-                }
-                if (pathGrid[startY][startX]-1==1) {
-                    moveSafety.left=true
-                    console.log("left: "+moveSafety);
-                }
-            }
-            // if (startY+1<=10||startY-1>=0) {
-            //     if (pathGrid[startY+1]==1) {
-            //         moveSafety.up=true
-            //         console.log("no up: "+moveSafety);
-            //     }
-            //     if (pathGrid[startY-1]==1) {
-            //         moveSafety.down=true
-            //         console.log("no down: "+moveSafety);
-            //     }
-            // }
-        // }
-        return moveSafety
-    }
+     //Fill Next row
+     fill(grid, sR + 1, sC, newC, origC);
+
+     //Fill Prev col
+     fill(grid, sR, sC - 1, newC, origC);
+
+     //Fill next col
+     fill(grid, sR, sC + 1, newC, origC);
+}
+
+// function movePicker(pathGrid,startY,startX,moveSafety) {
+//     console.log(moveSafety);
+    
+//     // for (let m = 0; m < 4; m++) {
+//         // console.log(pathGrid[startY][startX+1]);
+//         if (startX+1<=10||startX-1>=0) {
+//             if (pathGrid[startY][startX]+1==1) {
+//                 moveSafety.right=true
+//                 console.log("right: "+moveSafety);
+                
+//             }
+//             if (pathGrid[startY][startX]-1==1) {
+//                 moveSafety.left=true
+//                 console.log("left: "+moveSafety);
+//             }
+//         }
+//         // if (startY+1<=10||startY-1>=0) {
+//         //     if (pathGrid[startY+1]==1) {
+//         //         moveSafety.up=true
+//         //         console.log("no up: "+moveSafety);
+//         //     }
+//         //     if (pathGrid[startY-1]==1) {
+//         //         moveSafety.down=true
+//         //         console.log("no down: "+moveSafety);
+//         //     }
+//         // }
+//     // }
+//     return moveSafety
+// }
 
 export default function move(gameState){
     // console.log(grid);
@@ -122,108 +98,97 @@ export default function move(gameState){
     const myNeck = gameState.you.body[1];
     // const myBody = gameState.you.body;
     let moveSafety = {
-        up: false,
-        down: false,
-        left: false,
-        right: false
+        up: true,
+        down: true,
+        left: true,
+        right: true
     };
-    
-    // let sRow = myHead.y
-    // let sClm = myHead.x
-    // let newColor = 2
-    // let foodD = floodFill(grid, sRow, sClm, newColor);
-    // console.log(foodD);
-    
-    
-    // if (myNeck.x < myHead.x) {        // Neck is left of head, don't move left
-    //     moveSafety.left = false;
+
+    if (myNeck.x < myHead.x) {        // Neck is left of head, don't move left
+        moveSafety.left = false;
         
-    // } else if (myNeck.x > myHead.x) { // Neck is right of head, don't move right
-    //     moveSafety.right = false;
+    } else if (myNeck.x > myHead.x) { // Neck is right of head, don't move right
+        moveSafety.right = false;
         
-    // } else if (myNeck.y < myHead.y) { // Neck is below head, don't move down
-    //     moveSafety.down = false;
+    } else if (myNeck.y < myHead.y) { // Neck is below head, don't move down
+        moveSafety.down = false;
         
-    // } else if (myNeck.y > myHead.y) { // Neck is above head, don't move up
-    //     moveSafety.up = false;
-    // } 
+    } else if (myNeck.y > myHead.y) { // Neck is above head, don't move up
+        moveSafety.up = false;
+    } 
     
     // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
     // gameState.board contains an object representing the game board including its width and height
     // https://docs.battlesnake.com/api/objects/board
     // gameState.board
-    // console.log(gameState.board.height);
-    // if (myHead.x + 1 > gameState.board.width-1) {
-    //     console.log("no right");
-    //     moveSafety.right = false
-    // }
-    // if (myHead.x - 1 < 0) {
-    //     console.log("no left");
-    //     moveSafety.left = false
-    // }
-    // if (myHead.y + 1 > gameState.board.height-1) {
-    //     console.log("no up");
-    //     moveSafety.up = false
-    // }
-    // if (myHead.y - 1 < 0) {
-    //     console.log("no down");
-    //     moveSafety.down = false
-    // }
+    console.log(gameState.board.height);
+    if (myHead.x + 1 > gameState.board.width-1) {
+        console.log("no right");
+        moveSafety.right = false
+    }
+    if (myHead.x - 1 < 0) {
+        console.log("no left");
+        moveSafety.left = false
+    }
+    if (myHead.y + 1 > gameState.board.height-1) {
+        console.log("no up");
+        moveSafety.up = false
+    }
+    if (myHead.y - 1 < 0) {
+        console.log("no down");
+        moveSafety.down = false
+    }
     // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     // gameState.you contains an object representing your snake, including its coordinates
     // https://docs.battlesnake.com/api/objects/battlesnake
-    // if (myHead.x>) {
-        
-    // }
-    // console.log();
     let myBody = gameState.you.body
-    // for (let i = 0; i < myBody.length; i++) {
-    //     let segment = myBody[i]
-    //     if (myHead.y == segment.y && myHead.x == segment.x-1) {
-    //         moveSafety.right = false
-    //     }
-    //     else if (myHead.y == segment.y && myHead.x == segment.x+1) {
-    //         moveSafety.left = false
-    //     }
+    for (let i = 0; i < myBody.length; i++) {
+        let segment = myBody[i]
+        if (myHead.y == segment.y && myHead.x == segment.x-1) {
+            moveSafety.right = false
+        }
+        else if (myHead.y == segment.y && myHead.x == segment.x+1) {
+            moveSafety.left = false
+        }
 
-    //     else if (myHead.x == segment.x && myHead.y == segment.y-1) {
-    //         moveSafety.up = false
-    //     }
-    //     else if (myHead.x == segment.x && myHead.y == segment.y+1) {
-    //         moveSafety.down = false
-    //     }
-    // }
+        else if (myHead.x == segment.x && myHead.y == segment.y-1) {
+            moveSafety.up = false
+        }
+        else if (myHead.x == segment.x && myHead.y == segment.y+1) {
+            moveSafety.down = false
+        }
+    }
     
     
     // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
     // gameState.board.snakes contains an array of enemy snake objects, which includes their coordinates
     // https://docs.battlesnake.com/api/objects/battlesnake
     let sneks = gameState.board.snakes
-    // for (let z = 1; z < sneks.length; z++) {
-    //     let snekC = sneks[z]
-    //     for (let s = 0; s < snekC.body.length; s++) {
-    //         snekBody = snekC.body[s]
-    //         snekHead = snekC.head
-    //         console.log("snake:"+snekC.head.x);
-    //         if (snekBody.x == myHead.x && snekBody.y+1 == myHead.y || snekHead.x == myHead.x && snekHead.y+2 == myHead.y) {
-    //             console.log(snekBody.x +" , "+ myHead.x);
-    //             moveSafety.down = false
-    //         }
-    //         if (snekBody.x == myHead.x && snekBody.y-1 == myHead.y || snekHead.x == myHead.x && snekHead.y-2 == myHead.y) {
-    //             console.log(snekBody.x +" , "+ myHead.x);
-    //             moveSafety.up = false
-    //         }
-    //         if (snekBody.y == myHead.y && snekBody.x-1 == myHead.x || snekHead.x-2 == myHead.x && snekHead.y == myHead.y) {
-    //             console.log(snekBody.x +" , "+ myHead.x);
-    //             moveSafety.right = false
-    //         }
-    //         if (snekBody.y == myHead.y && snekBody.x+1 == myHead.x || snekHead.x+2 == myHead.x && snekHead.y == myHead.y) {
-    //             console.log(snekBody.x +" , "+ myHead.x);
-    //             moveSafety.left = false
-    //         }
-    //         // gridMaker(snekBody, snekHead)
-    //     }
-    // }
+    for (let z = 1; z < sneks.length; z++) {
+        let snekC = sneks[z]
+        for (let s = 0; s < snekC.body.length; s++) {
+            let snekBody = snekC.body[s]
+            let snekHead = snekC.head
+            console.log("snake:"+snekC.head.x);
+            if (snekBody.x == myHead.x && snekBody.y+1 == myHead.y || snekHead.x == myHead.x && snekHead.y+2 == myHead.y) {
+                console.log(snekBody.x +" , "+ myHead.x);
+                moveSafety.down = false
+            }
+            if (snekBody.x == myHead.x && snekBody.y-1 == myHead.y || snekHead.x == myHead.x && snekHead.y-2 == myHead.y) {
+                console.log(snekBody.x +" , "+ myHead.x);
+                moveSafety.up = false
+            }
+            if (snekBody.y == myHead.y && snekBody.x-1 == myHead.x || snekHead.x-2 == myHead.x && snekHead.y == myHead.y) {
+                console.log(snekBody.x +" , "+ myHead.x);
+                moveSafety.right = false
+            }
+            if (snekBody.y == myHead.y && snekBody.x+1 == myHead.x || snekHead.x+2 == myHead.x && snekHead.y == myHead.y) {
+                console.log(snekBody.x +" , "+ myHead.x);
+                moveSafety.left = false
+            }
+            // gridMaker(snekBody, snekHead)
+        }
+    }
     let width = gameState.board.width;
     let height = gameState.board.height;
     let grid = gridMaker(height,width, myBody, sneks);
@@ -232,14 +197,17 @@ export default function move(gameState){
     let newColor = 2;
     let pathGrid = floodFill(grid,startX,startY,newColor);
     // console.log("path: "+pathGrid.length);
-    moveSafety = movePicker(pathGrid,startY,startX,moveSafety)
+    // moveSafety = movePicker(pathGrid,startY,startX,moveSafety)
+    for (let l = 0; l < pathGrid.length; l++) {
+        console.log(pathGrid);
+    }
     
     // console.log("grid"+grid[h]);
     
     // Are there any safe moves left?
     
     //Object.keys(moveSafety) returns ["up", "down", "left", "right"]
-    //.filter() filters the array based on the function provided as an argument (using arrow function syntax here)
+    //.filter() filters the array based on the function provided as an argument (using arsRow function syntax here)
     //In this case we want to filter out any of these directions for which moveSafety[direction] == false
     const safeMoves = Object.keys(moveSafety).filter(direction => moveSafety[direction]);
     console.log(safeMoves);
